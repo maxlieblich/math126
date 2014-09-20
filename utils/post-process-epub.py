95e7ac208b33689b5e8cb745580510603dea69ea
R. Andrew Ohana <andrew.ohana@gmail.com>
2014-09-20 00:31:16 -0700

    embed mathjax in epubs
    this should make math render properly

--- utils/post-process-epub.py
+++ utils/post-process-epub.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 import re
 import tempfile
@@ -10,17 +11,86 @@ except ImportError:
 
 import xml.etree.ElementTree as ElementTree
 
+MIME_TYPES = {
+        '.js': 'application/x-javascript',
+        '.woff': 'application/font-woff',
+        '.otf': 'font/opentype',
+        '.png': 'image/png',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.svg': 'image/svg+xml',
+        '.html': 'text/html',
+        '.md': 'text/x-markdown',
+        '.txt': 'text/plain',
+        '.json': 'application/json',
+        '.gif': 'image/gif',
+        }
+
+MATHJAX_CONFIG = r'''
+<script type="text/x-mathjax-config">
+//<![CDATA[
+MathJax.Hub.Config({
+    jax: [
+        "input/TeX",
+        "output/SVG",
+    ],
+    extensions: [
+        "tex2jax.js",
+        "MathEvents.js",
+    ],
+    TeX: {
+        extensions: [
+            "AMSmath.js",
+            "AMSsymbols.js",
+            "noErrors.js",
+            "noUndefined.js",
+            "autoload-all.js"
+        ]
+    },
+    MathMenu: {
+        showRenderer: false
+    },
+    menuSettings: {
+        zoom: "Click"
+    },
+    messageStyle: "none"
+});
+//]]>
+</script>
+'''
+MATHJAX_SCRIPT = r'<script type="text/javascript" src="MathJax/MathJax.js" />'
+MATHJAX_FILES = (
+        'MathJax.js',
+        'images',
+        'jax/element',
+        'jax/input/TeX',
+        'jax/output/SVG/autoload',
+        'jax/output/SVG/config.js',
+        'jax/output/SVG/fonts/TeX',
+        'jax/output/SVG/jax.js',
+        'extensions/TeX',
+#        'extensions/FontWarnings.js',
+#        'extensions/HelpDialog.js',
+        'extensions/MathEvents.js',
+        'extensions/MathMenu.js',
+        'extensions/MathZoom.js',
+        'extensions/tex2jax.js',
+        )
+
 EPUB_URI = 'http://www.idpf.org/2007/opf'
 XHTML_URI = 'http://www.w3.org/1999/xhtml'
 
 CHAPTER_REGEX = re.compile(r'ch\d+\.xhtml')
 
 def parse_xml(file, uri=XHTML_URI):
+    # the ElementTree module seems poorly written, with registering
+    # namespaces globally (it should be able to figure out the namespaces
+    # from parsing the xml correctly)
     ElementTree.register_namespace('', uri)
     if uri == XHTML_URI:
         ElementTree.register_namespace('epub', EPUB_URI)
 
     xmltree = ElementTree.parse(file)
+    # silly hack to deal with ElementTree ignoring multiple namespaces
     if uri == XHTML_URI:
         xmltree.getroot().set('xmlns:epub', EPUB_URI)
 
@@ -49,9 +119,15 @@ class EpubUpdater:
         self._closed = True
 
     def _write_xml(self, path, xmltree, uri=XHTML_URI):
+        # the ElementTree module seems poorly written, with registering
+        # namespaces globally (it should be able to figure out the namespaces
+        # from parsing the xml correctly)
         ElementTree.register_namespace('', uri)
         if uri == XHTML_URI:
             ElementTree.register_namespace('epub', EPUB_URI)
+
+        # as far as I can tell you have to dump to a file to get the
+        # xml_declaration, so we use BytesIO to emulate a file
         buf = BytesIO()
         xmltree.write(buf, encoding='UTF-8', xml_declaration=True)
         self._new.writestr(self._old.getinfo(path), buf.getvalue())
@@ -64,36 +140,49 @@ class EpubUpdater:
             href = item.get('href')
             if not CHAPTER_REGEX.match(href):
                 continue
-            item.set('properties', 'scripted')
+
             xmltree = parse_xml(self._old.open(href))
             head = xmltree.find('{%s}head'%XHTML_URI)
             for header in self._extra_headers:
                 head.append(header)
             self._write_xml(href, xmltree)
 
-    def _copy_missing(self):
-        updated = {f.filename for f in self._new.filelist}
-        for f in self._old.filelist:
-            if f.filename not in updated:
-                self._new.writestr(f, self._old.read(f))
+            item.set('properties', 'scripted')
 
-    def add_scripts(self, scripts):
+    def _copy_missing(self):
+        updated = {info.filename for info in self._new.filelist}
+        for info in self._old.filelist:
+            if info.filename not in updated:
+                self._new.writestr(info, self._old.read(info))
+
+    def _add_files(self, files, path_mutator=lambda path: path):
         paths = set()
-        for script in scripts:
-            path = os.path.join('js', os.path.basename(script))
-            self._new.write(script, path, zipfile.ZIP_DEFLATED)
+        for opath in files:
+            if os.path.basename(opath) == '.DS_Store':
+                # TODO: make this check a regex
+                # since we will want to skip other silly OS/application files
+                continue
+            path = path_mutator(opath)
+            self._new.write(opath, path, zipfile.ZIP_DEFLATED)
             paths.add(path)
 
         for path in paths:
             self._manifest.append(
                     ElementTree.SubElement(self._manifest, 'item',
                         attrib = {
-                            'id': path.replace('/','_').replace('.','_'),
+                            'id': path.replace(os.sep, '_').replace('.', '_'),
                             'href': path,
-                            'media-type': 'text/javascript',
+                            'media-type': MIME_TYPES[os.path.splitext(path)[1]],
                             },
                         )
                     )
+
+    def _add_file(self, file, *args, **kwds):
+        self._add_files((file,), *args, **kwds)
+
+    def add_scripts(self, scripts):
+        self._add_files(scripts,
+                lambda path: os.path.join('js', os.path.basename(path)))
 
     def add_script(self, script):
         self.add_scripts((script,))
@@ -104,6 +193,23 @@ class EpubUpdater:
 
     def add_header(self, header):
         self.add_headers((header,))
+
+    def embed_mathjax(self, mathjax):
+        paths = set()
+        for path in MATHJAX_FILES:
+            path = os.path.join(mathjax, path)
+            if os.path.isdir(path):
+                for root, _, files in os.walk(path):
+                    paths.update(
+                        itertools.imap(
+                            lambda path: os.path.join(root, path), files)
+                        )
+            else:
+                paths.add(path)
+
+        regex = re.compile('^' + mathjax)
+        self._add_files(paths, lambda path: regex.sub('MathJax', path))
+        self.add_headers((MATHJAX_CONFIG, MATHJAX_SCRIPT))
 
 def create_parser():
     import argparse
@@ -124,8 +230,8 @@ if __name__ == '__main__':
         epub = EpubUpdater(args.input, args.output)
         epub.add_scripts(args.scripts)
 
-        #if args.mathjax:
-        #    epub.embed_mathjax()
+        if args.mathjax:
+            epub.embed_mathjax(args.mathjax)
         if args.include_in_headers:
             epub.add_headers(open(args.include_in_headers))
 
