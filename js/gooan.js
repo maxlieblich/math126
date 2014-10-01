a539d6381b4551587bc1e67fc7e56f7b1a794c0b
max <max@lieblich.us>
2014-10-01 11:47:31 -0700

    Update Website

---
+++ js/gooan.js
@@ -0,0 +1,11 @@
+/* Google Analytics. Need this up here so that we can use ga to record events in MathScene, etc. 
+    If you fork this, please do not use this tracking code!
+    */
+
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-55020935-1', 'auto');
+    ga('require', 'displayfeatures');
+    ga('send', 'pageview');
